2eac2347b976e91376473aaf87a371bdc4c72fae
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-01-19 21:51:08 -0300

    chore: remove legacy static project to start NEWt Weather App

--- js/date.js
+++
@@ -1,4 +0,0 @@
-const date = new Date();
-const currentYear = date.getFullYear();
-
-document.getElementById('currentYearDate').innerHTML = '© '+currentYear+' ismaelmarot.com.ar'; 
--- js/hide-menu.js
+++
@@ -1,5 +0,0 @@
-function hideMenu(){
-	document.getElementById('input-check-btn-menu').checked = false;
-}
-
-window.addEventListener('scroll',hideMenu);
