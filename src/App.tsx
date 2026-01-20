767ff04a22c05b4dc53efd25b8fe4bbb3356b066
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-01-20 10:25:18 -0300

    refactor: introduce base project structure and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import Home from './pages/Home'
 import { GlobalStyles } from './styles/GlobalStyles'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div className="container min-vh-100 d-flex align-items-center justify-content-center">
-        <h1 className='text-center'>
-         Weather App
-        </h1>
-      </div>
+        <Home />
     </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="container min-vh-100 d-flex align-items-center justify-content-center">
+      <h1 className="text-center">Weather App</h1>
+    </div>
+  )
+}
+
+export default Home
