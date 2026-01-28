3d6fc76b7adc3f807782ffb671921f0295ada07b
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-01-28 16:40:46 -0300

    index for constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export { CARDINALS } from './Cardinals'
+export { GeneralColors } from './GeneralColors'
+export { windDeg } from './WindDeg'
---
+++ src/hooks/index.ts
@@ -0,0 +1,2 @@
+export { useGeolocation } from './useGeolocation'
+export { useWeather } from './useWeather'
