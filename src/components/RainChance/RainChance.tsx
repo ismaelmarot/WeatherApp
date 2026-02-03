84e3a7eb6ffa69bea3e4ea0218738fa4551035c6
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-03 18:37:04 -0300

    restyle: RainChance

--- src/components/RainChance/RainChance.tsx
+++ src/components/RainChance/RainChance.tsx
@@ -1,34 +1,30 @@
+import type { RainChanceProps } from '../../types/RainChance.type';
 import {
-  Container,
-  Title,
-  Bar,
-  Fill,
-  Value,
-  Badge
+    Container,
+    Title,
+    Bar,
+    Fill,
+    Value,
+    Badge
 } from './RainChance.style';
 
-type RainChanceProps = {
-  chance: number;
-  isRainingNow?: boolean;
-};
-
 export function RainChance({
-  chance,
-  isRainingNow = false
+    chance,
+    isRainingNow = false
 }: RainChanceProps) {
-  const safeChance = Math.min(Math.max(chance, 0), 100);
+    const safeChance = Math.min(Math.max(chance, 0), 100);
 
-  return (
-    <Container>
-      <Title>Chance of rain</Title>
+    return (
+        <Container>
+            <Title>Chance of rain</Title>
 
-      <Bar>
-        <Fill value={safeChance} />
-      </Bar>
+            <Bar>
+                <Fill value={safeChance} />
+            </Bar>
 
-      <Value>{safeChance}%</Value>
+            <Value>{safeChance}%</Value>
 
-      {isRainingNow && <Badge>Raining now</Badge>}
-    </Container>
-  );
+            {isRainingNow && <Badge>Raining now</Badge>}
+        </Container>
+    );
 }
---
+++ src/types/RainChance.type.ts
@@ -0,0 +1,4 @@
+export type RainChanceProps = {
+    chance: number;
+    isRainingNow?: boolean;
+};
