a85a6dfd94991743293f75aea596ff40acc053a1
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-08 17:33:23 -0300

    restyle: MobileScreen2 , MobileScreen3

--- src/screens/mobile/MobileScreen2.tsx
+++ src/screens/mobile/MobileScreen2.tsx
@@ -1,10 +1,27 @@
-import { PlaceholderBlock } from '../../components'
-import { ScreenLayoutBase } from '../../layouts/ScreenLayoutBase'
+// import { PlaceholderBlock } from '../../components'
+// import { ScreenLayoutBase } from '../../layouts/ScreenLayoutBase'
+
+// export function MobileScreen2() {
+//     return (
+//         <ScreenLayoutBase>
+//             <PlaceholderBlock label="🌡️ Temperatura actual" />
+//         </ScreenLayoutBase>
+//     )
+// }
+
+import { ScreenLayoutBase } from '../../layouts/ScreenLayoutBase';
+import { WeatherCurrent } from '../../components';
+import { useWeatherContext } from '../../context/WeatherContext';
 
 export function MobileScreen2() {
-    return (
-        <ScreenLayoutBase>
-            <PlaceholderBlock label="🌡️ Temperatura actual" />
-        </ScreenLayoutBase>
-    )
+  const { weather } = useWeatherContext();
+
+  // Si no hay datos, podemos mostrar un mensaje o placeholder
+  if (!weather) return null;
+
+  return (
+    <ScreenLayoutBase>
+      <WeatherCurrent weather={weather} />
+    </ScreenLayoutBase>
+  );
 }
--- src/screens/mobile/MobileScreen3.tsx
+++ src/screens/mobile/MobileScreen3.tsx
@@ -1,11 +1,17 @@
-import { PlaceholderBlock } from '../../components'
 import { ScreenLayoutBase } from '../../layouts/ScreenLayoutBase'
+import { UvIndex, Humidity, PressureValue } from '../../components'
+import { useWeatherContext } from '../../context/WeatherContext'
 
 export function MobileScreen3() {
+    const { weather } = useWeatherContext();
+
+    if (!weather) return null;
+
     return (
         <ScreenLayoutBase>
-            <PlaceholderBlock label="💨 Viento" />
-            <PlaceholderBlock label="📈 Presión" />
+            <UvIndex value={weather.current.uv} />
+            <Humidity value={weather.current.humidity} />
+            <PressureValue value={weather.current.pressure_mb} />
         </ScreenLayoutBase>
-  )
+    )
 }
