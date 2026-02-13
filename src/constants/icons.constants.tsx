4ac811851f5f40fcc021d55373ec75eea6d05d28
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-13 20:08:35 -0300

    add: react icons

--- src/constants/icons.constants.tsx
+++ src/constants/icons.constants.tsx
@@ -1,7 +1,27 @@
 import { FaLocationArrow } from 'react-icons/fa'
 import { IoLocationSharp } from 'react-icons/io5'
+import { TbTemperatureMinus } from 'react-icons/tb'
+import { TbTemperaturePlus } from 'react-icons/tb'
+import { FiSunrise } from 'react-icons/fi'
+import { FiSunset } from 'react-icons/fi'
+import { IoRainyOutline } from 'react-icons/io5'
+import { TbUvIndex } from 'react-icons/tb'
+import { WiHumidity } from 'react-icons/wi'
+import { MdOutlineVisibility } from 'react-icons/md'
+import { FaWind } from 'react-icons/fa'
+import { IoMdInformationCircleOutline } from 'react-icons/io'
 
 export const ICONS = {
     location: IoLocationSharp,
-    WindArrow: FaLocationArrow,
+    windArrow: FaLocationArrow,
+    tempMin: TbTemperatureMinus,
+    tempMax: TbTemperaturePlus,
+    sunrise: FiSunrise,
+    sunset: FiSunset,
+    rain: IoRainyOutline,
+    uvIndex: TbUvIndex,
+    humidity: WiHumidity,
+    visibility: MdOutlineVisibility,
+    wind: FaWind,
+    info: IoMdInformationCircleOutline,
 }
