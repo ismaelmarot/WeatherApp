c46a62c9032f9bc88ab433cac4c7f26d29c40d0d
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-14 09:52:38 -0300

    restyle: InfoPopup design

--- src/components/InfoPopup/InfoPopup.style.ts
+++ src/components/InfoPopup/InfoPopup.style.ts
@@ -3,63 +3,47 @@ import { GeneralColors } from '../../constants'
 import { flex, size } from '../../mixins'
 
 export const Overlay = styled.div`
-  position: fixed;
-  inset: 0;
-  z-index: 11;
-
-  /* background: rgb(20, 97, 107); */
-  backdrop-filter: blur(8px);
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    ${flex('column','center','center')}
+    position: fixed;
+    inset: 0;
+    z-index: 11;
+    backdrop-filter: blur(8px);
 `
 
 export const Popup = styled.div`
+    ${flex('column','center','center')}
     ${size('100%','100%')}
-  border-radius: .5rem;
-
-  background: ${GeneralColors.grey};
-
-  display: flex;
-  flex-direction: column;
+    border-radius: .5rem;
+    background: ${GeneralColors.white};
 `
 
-/* HEADER NO SCROLLEA */
 export const Header = styled.div`
-    ${flex('column','center','space-between')}
-  position: sticky;
-  top: 0;
-  z-index: 10;
-
-  height: 56px;
-  padding: 0 1rem;
-
-  background: ${GeneralColors.grey};
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    ${flex('row','center','space-between')}
+    ${size('95%','3rem')}
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    padding: 0 1rem;
+    border-bottom: 2px solid rgba(0, 0, 0, 0.1);
 `
 
 export const Title = styled.h3`
-  margin: 0;
-  font-size: 1.1rem;
-  color: ${GeneralColors.black};
+    margin: 0;
+    font-size: 1.1rem;
+    color: ${GeneralColors.black};
 `
 
 export const CloseButton = styled.button`
-  border: none;
-  background: none;
-
-  font-size: 1.4rem;
-  font-weight: bold;
-  cursor: pointer;
-
-  line-height: 1;
+    border: none;
+    background: none;
+    font-size: 1.4rem;
+    font-weight: bold;
+    cursor: pointer;
+    line-height: 1;
 `
 
-/* ESTE ES EL ÚNICO SCROLL */
 export const ScrollContent = styled.div`
-  flex: 1;
-  overflow-y: auto;
-
-  padding: 1rem;
+    flex: 1;
+    overflow-y: auto;
+    padding: 1rem;
 `
