85ef60aff1a50b9983a1d05b795810bf298c0750
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-14 22:15:22 -0300

    refact icons

--- src/constants/icons.constants.tsx
+++ src/constants/icons.constants.tsx
@@ -1,15 +1,15 @@
+import { FiSunrise, FiSunset } from 'react-icons/fi'
 import { FaLocationArrow } from 'react-icons/fa'
 import { IoLocationSharp } from 'react-icons/io5'
 import { TbTemperatureMinus } from 'react-icons/tb'
 import { TbTemperaturePlus } from 'react-icons/tb'
-import { FiSunrise } from 'react-icons/fi'
-import { FiSunset } from 'react-icons/fi'
 import { IoRainyOutline } from 'react-icons/io5'
 import { TbUvIndex } from 'react-icons/tb'
 import { WiHumidity } from 'react-icons/wi'
 import { MdOutlineVisibility } from 'react-icons/md'
 import { FaWind } from 'react-icons/fa'
 import { IoMdInformationCircleOutline } from 'react-icons/io'
+import { FaCircle } from 'react-icons/fa'
 
 export const ICONS = {
     location: IoLocationSharp,
@@ -24,4 +24,5 @@ export const ICONS = {
     visibility: MdOutlineVisibility,
     wind: FaWind,
     info: IoMdInformationCircleOutline,
+    circle: FaCircle,
 }
