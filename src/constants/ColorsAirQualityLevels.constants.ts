1c47d689d10abe6ba9a48d4cc6e650a06dc53b70
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-16 21:21:32 -0300

    Add: ColorsAirQualityLevels.constants.ts

---
+++ src/constants/ColorsAirQualityLevels.constants.ts
@@ -0,0 +1,7 @@
+export const ColorsAirQualityLevels = {
+    Verde:'rgba(34, 197, 94, 1)',
+    Amarillo: 'rgba(250, 204, 21, 1)',
+    Naranja: 'rgba(249, 115, 22, 1)',
+    Violeta: 'rgba(139, 92, 246, 1)',
+    Marrón: 'rgba(146, 64, 14, 1)',
+}
