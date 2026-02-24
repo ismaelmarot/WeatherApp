f4a7b313ecf992da3fd0536573753bf7fea6cbe2
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-24 20:55:10 -0300

    restyle: MobileScreen5 > tablet layout

--- src/screens/mobile/MobileScreen5/MobileScreen5.style.ts
+++ src/screens/mobile/MobileScreen5/MobileScreen5.style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
+import { BREAKPOINTS } from '../../../constants'
 
 export const Container = styled.div`
     height: 37rem;
+    @media (min-width: ${BREAKPOINTS.tablet}) {
+        width: 95%;
+    }
 `
