e0bfa9925bef4f9b3fbbc67c8fdd2eb77b4e8554
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-02-27 19:41:49 -0300

    changes before rebase

--- src/utils/forecast/_test_/getNextHours.utils.test.ts
+++ src/utils/forecast/_test_/getNextHours.utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
-import { getNextHours } from '../getNextHours.utils'
+import { getNextHours } from '../getNextOurs.utils'
 
 describe('getNextHours', () => {
     const mockNow = new Date('2026-02-23T12:00:00')
